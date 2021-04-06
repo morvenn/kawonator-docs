760e60c2888c8b69f103d1d3dc909665408ebdbf
morvenn <morvenn@users.noreply.github.com>
2021-04-06 12:40:57 +0200

    output from dita project

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,19 @@
+define(function() {var keywords=[{w:"Glossary",p:["p0"]},{w:"Consumer",p:["p1"]},{w:"guide",p:["p1","p2"]},{w:"Maintenance",p:["p2"]},{w:"Coffees",p:["p3"]},{w:"to",p:["p3"]},{w:"try",p:["p3"]},{w:"Assembling",p:["p4","p5"]},{w:"the",p:["p4","p5","p6","p7","p8","p9","p10","p11"]},{w:"Kawomator",p:["p4","p5","p10","p11"]},{w:"KW100",p:["p4","p6","p8","p11"]},{w:"KW200",p:["p5","p7","p9","p10"]},{w:"Cleaning",p:["p6","p7","p8","p9"]},{w:"filter",p:["p6","p7"]},{w:"holder",p:["p6","p7"]},{w:"and",p:["p6","p7","p8","p9"]},{w:"portafilter",p:["p6","p7"]},{w:"steam",p:["p8","p9"]},{w:"wand",p:["p8","p9"]},{w:"frothing",p:["p8","p9"]},{w:"attachment",p:["p8","p9"]},{w:"Operating",p:["p10","p11"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1, 2];
+ph["p11"]=[21, 8, 9, 10];
+ph["p2"]=[3, 2];
+ph["p3"]=[4, 5, 6];
+ph["p4"]=[7, 8, 9, 10];
+ph["p5"]=[7, 8, 9, 11];
+ph["p6"]=[12, 8, 10, 13, 14, 15, 16];
+ph["p7"]=[12, 8, 11, 13, 14, 15, 16];
+ph["p8"]=[12, 8, 10, 17, 18, 15, 19, 20];
+ph["p9"]=[12, 8, 11, 17, 18, 15, 19, 20];
+ph["p10"]=[21, 8, 9, 11];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {13:2,5:2,4:2,9:2,3:1,1:-1,2:-1,8:2,11:1,7:2,10:1,12:2,6:2};
+});
